64b97889818ec47751f274b592c20e67b5704852
gerwayne <gerwayneyewrui16@gmail.com>
2024-05-27 16:20:01 +0800

    chat design complete

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Sidenav from './navbar/Sidenav';
+import Chathome from './components/chat/Chathome';
 
 export const Homepage = () => {
     return (
         <div>This is supposed be the homepage btw
             <Sidenav />
+
+            <Chathome />
         </div>
     )
 }
